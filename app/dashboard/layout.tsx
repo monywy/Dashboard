b0f34ad4a7cdaed9ecfe723063eec01822a63672
mony.wy.100@gmail.com <mony.wy.100@gmail.com>
2024-02-06 15:17:27 -0500

    Invioces Update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,8 +10,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+   <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+    <div className='w-full flex-none md:w-64'>
+    <SideNav />
+    </div>
+    <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+   </div>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,8 +7,37 @@ import {fetchCardData} from '@/app/lib/data';
 import { Suspense } from "react";
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
-import SideNav from '@/app/ui/dashboard/sidenav';
- 
-export default function Page() {
-  return <p>Dashboard Page</p>;
+export default async function Page(){
+    
+    
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+      } = await fetchCardData();
+    
+
+    return (
+        <main>
+          <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            Dashboard
+          </h1>
+          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+          </div> 
+          <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            
+            <Suspense fallback={<RevenueChartSkeleton/>}>
+             <RevenueChart /> 
+             </Suspense>
+
+             <Suspense fallback={<LatestInvoicesSkeleton/>}>
+            <LatestInvoices /> 
+            </Suspense>
+          </div>
+        </main>
+      );
 }
