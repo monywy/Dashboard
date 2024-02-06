085d2e0a1814c7c08aeb2bb470844e23e6526d2f
mony.wy.100@gmail.com <mony.wy.100@gmail.com>
2024-02-06 15:16:37 -0500

    Invioces Uodate

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,17 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-export default function Layout(
-{children}: {children: React.ReactNode}
-) 
+import '@/app/ui/global.css';
+import { lusitana } from '@/app/ui/fonts';
+ 
 
-{
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-    <div className="w-full flex-none md:w-64">
-      <SideNav />
-    </div>
-    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-  </div>
-);
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import {Montserrat,Lusitana} from 'next/font/google'
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
 
 export const montserrat = Montserrat({subsets: ['latin']})
 
